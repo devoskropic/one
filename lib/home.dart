8113821e69d7a4b442d4d7324377e4f90fdb8bf6
diegoskropic <diego.delarosa@hotmail.es>
2022-01-29 00:10:40 -0600

    bottom navbar

--- lib/home.dart
+++ lib/home.dart
@@ -1,30 +1,47 @@
 import 'package:flutter/material.dart';
 
-// 1
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
+
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
   // HW: Add state variables and functions
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
           'Hablemo',
-          // 2
           style: Theme.of(context).textTheme.headline6,
         ),
       ),
       // HW: Show selected tab
       body: Center(
-          child: Text('Let\'s get cooking ! ',
-              // 3
-              style: Theme.of(context).textTheme.headline1)),
+        child: Text('Let\'s get cooking!',
+            style: Theme.of(context).textTheme.headline1),
+      ),
       // HW: Add bottom navigation bar
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: "Card",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: "Card2",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: "Card3",
+          ),
+        ],
+      ),
     );
   }
 }
